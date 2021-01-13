f2a29ffeae2edd93713e985f5f9599c3e5678588
Alex Toma <52259857+alexandrutm@users.noreply.github.com>
2021-01-13 13:36:32 +0200

    Revert "Revert "Add a new view""
    This reverts commit 28262d1520bedc942c8c4fea3a3f1f38999bb4f2.

---
+++ Orar/OrarApp/RoomView.cpp
@@ -0,0 +1,39 @@
+#include"stdafx.h"
+#include "RoomView.h"
+#include "Context.h"
+#include"INavigator.h"
+
+RoomView::RoomView(INavigator* aNavigator, Context& aContext, QWidget* parent)
+	: QWidget(parent), mNavigator(aNavigator), mContext(aContext)
+{
+	ui.setupUi(this);
+}
+
+RoomView::~RoomView()
+{
+}
+void RoomView::on_mAdd_clicked()
+{
+
+}
+void RoomView::on_mEdit_clicked()
+{
+}
+
+void RoomView::on_mDelete_clicked()
+{
+}
+
+void RoomView::on_mConstraints_clicked()
+{
+}
+
+void RoomView::on_mNext_clicked()
+{
+	mNavigator->ChangeView(INavigator::viewId::teacherView);
+}
+
+void RoomView::on_mBack_clicked()
+{
+	mNavigator->ChangeView(INavigator::viewId::classesView);
+}

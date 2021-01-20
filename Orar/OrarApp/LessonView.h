1cc94a2303bce787b47a85820fce6e1a5469fb3a
Alex Toma <52259857+alexandrutm@users.noreply.github.com>
2021-01-20 11:43:18 +0200

    Revert "Changed tablewidget with table view  and share the same model between table views and comboboxes from  leson dialog"
    This reverts commit 30c63ca8bc0323ccd061c6beede56462627745de.

--- Orar/OrarApp/LessonView.h
+++ Orar/OrarApp/LessonView.h
@@ -6,12 +6,6 @@
 class INavigator;
 class Context;
 
-class TableModel;
-class SubjectTableModel;
-class ClassTableModel;
-class RoomTableModel;
-class LessonTableModel;
-
 class LessonView : public QWidget
 {
 	Q_OBJECT
@@ -19,6 +13,9 @@ class LessonView : public QWidget
 public:
 	LessonView(INavigator* aNavigator, Context& aContext,QWidget *parent = Q_NULLPTR);
 	~LessonView();
+	void ClearData();
+	void UpdateTable();
+	void PopulateDialog(QString instuction);
 
 private slots:
 
@@ -32,12 +29,5 @@ private slots:
 private:
 	Ui::LessonView ui;
 	INavigator* mNavigator;
-
-	TableModel* mTeacherModel;
-	SubjectTableModel* mSubjectModel;
-	ClassTableModel* mClassModel;
-	RoomTableModel* mRoomTableModel;
-	LessonTableModel* tableModel;
-
 	Context& mContext;
 };

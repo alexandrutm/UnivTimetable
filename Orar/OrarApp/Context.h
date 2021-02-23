3e8c31f1b09e49244420f57a70b243dabb46ecfd
Alex Toma <52259857+alexandrutm@users.noreply.github.com>
2021-02-23 21:56:24 +0200

    Changed the way we get data from context

--- Orar/OrarApp/Context.h
+++ Orar/OrarApp/Context.h
@@ -14,40 +14,40 @@ public:
   void                          AddTeacher(shared_ptr<Teacher> aTeacher);
   void                          RemoveTeacher(int i);
   vector<shared_ptr<Teacher>> & GetTeachers();
-  int                           GetTeacherSize();
-  shared_ptr<Teacher>           GetTeacherByIndex(int i);
+  size_t                        GetTeacherSize();
+  shared_ptr<Teacher> &         GetTeacherByIndex(int i);
   void                          DeleteTeachers();
   int                           GenerateTeacherId();
 
   void                          AddSubject(shared_ptr<Subject> aSubject);
   void                          RemoveSubject(int i);
   vector<shared_ptr<Subject>> & GetSubjects();
-  int                           GetSubjectSize();
-  shared_ptr<Subject>           GetSubjectByIndex(int i);
+  size_t                        GetSubjectSize();
+  shared_ptr<Subject> &         GetSubjectByIndex(int i);
   void                          DeleteSubjects();
   int                           GenerateSubjectId();
 
   void                          AddClass(shared_ptr<Classes> aClass);
   void                          RemoveClass(int i);
   vector<shared_ptr<Classes>> & GetClasses();
-  int                           GetClassSize();
-  shared_ptr<Classes>           GetClassByIndex(int i);
+  size_t                        GetClassSize();
+  shared_ptr<Classes> &         GetClassByIndex(int i);
   void                          DeleteClasses();
   int                           GenerateClassId();
 
   void                         AddLesson(shared_ptr<Lesson> aLesson);
   void                         RemoveLesson(int i);
   vector<shared_ptr<Lesson>> & GetLessons();
-  int                          GetLessonSize();
-  shared_ptr<Lesson>           GetLessonByIndex(int i);
+  size_t                       GetLessonSize();
+  shared_ptr<Lesson> &         GetLessonByIndex(int i);
   void                         DeleteLessons();
   int                          GenerateLessonId();
 
   void                       AddRoom(shared_ptr<Room> aRoom);
   void                       RemoveRoom(int i);
   vector<shared_ptr<Room>> & GetRooms();
-  int                        GetRoomSize();
-  shared_ptr<Room>           GetRoomByIndex(int i);
+  size_t                     GetRoomSize();
+  shared_ptr<Room> &         GetRoomByIndex(int i);
   void                       DeleteRooms();
   int                        GenerateRoomId();
 

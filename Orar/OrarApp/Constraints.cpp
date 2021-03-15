c9156e7e943bbf1031e5390a06ea6618048a46cc
Alex Toma <52259857+alexandrutm@users.noreply.github.com>
2021-03-15 10:43:20 +0200

    Rename file

--- Orar/OrarApp/Constraints.cpp
+++
@@ -1,5 +0,0 @@
-#include "Constraints.h"
-
-Constraints::~Constraints()
-{
-}
--- Orar/OrarApp/Constraints.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Constraints
-{
-public:
-  virtual ~Constraints()  = 0;
-  virtual bool Validate() = 0;
-};
